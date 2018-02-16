58de36ad7efebf1cf56e7450f7df3cac06fa1fef
simonvalentin <simon.valentin@uni-konstanz.de>
2018-02-16 13:36:22 +0100

    Added data simulation and forest plot

--- ShinyMeta/server.R
+++ ShinyMeta/server.R
@@ -1,9 +1,8 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
@@ -11,108 +10,265 @@ library(shiny)
 library(metafor)
 library(DT)
 
-# Define server logic required to draw a histogram
+
+# TODO
+# - [x] Example data (bcg, alloc column) S + M
+# - [ ] Description box on data selection page (cite metafor) J
+# - [x] Odds ratio decimal places slider S
+# - [ ] Select variables from imported data and write tests J
+# - [x] Info/value boxes for suppl. info (links to packages, interesting articles, etc.) M + S
+# - [ ] Document contributions
+# - [x] Data simultion S
+
+
+
+# Define server logic
 shinyServer(function(input, output) {
- 
-  dataC <- reactive({input$dataChoice})
+  simulate_data = function(n, m, sd, or) {
+    # Generate random meta analysis data
+    # n: total number of studies to simulate
+    # m: mean sample size of studies
+    # sd: standard deviation of sample size of studies
+    # or:  odds ratio
+    
+    random_freqs = function(n, M1, M2, OR) {
+      # Return a vector of bivariate binary data as a function of odds ratio
+      # Implements the inverse function of the calculation of odds ratios from cell probabilities
+      # n: total number of observations
+      # M1: row marginal probabiliy
+      # M2: column marginal probability
+      # OR: odds ratio
+      # adapted from: https://stats.stackexchange.com/questions/13193/generating-data-with-a-pre-specified-odds-ratio
+      
+      # find value for p11
+      g = function(prob) {
+        log(prob) + log(1 - M1 - M2 + prob) - log(M1 - prob) - log(M2 - prob) - log(OR)
+      }
+      br = c(max(0, M1 + M2 - 1), min(M1, M2))
+      p11 = uniroot(g, br)$root # convenient numiercal solution
+      
+      # fill in other cell probabilities
+      p10 = M1 - p11
+      p01 = M2 - p11
+      p00 = 1 - p11 - p10 - p01
+      
+      # generate random numbers with corresponding cell probabilities
+      x = runif(n)
+      n11 = sum(x < p11)
+      n10 = sum(x < (p11 + p10)) - n11
+      n01 = sum(x < (p11 + p10 + p01)) - n11 - n10
+      n00 = n - (n11 + n10 + n01)
+      
+      freqs = c(n11, n10, n01, n00)
+      return(freqs)
+    }
+    
+    # Generate random studies
+    # Note: We're assuming a log-normal distribution of the sample sizes here,
+    # other distributions would also be possible.
+    
+    n_studies <- n # total number of studies to simulate
+    sample_size_mean <- floor(log(m))  # mean sample size of studies
+    sample_size_sd <-
+      floor(log(sd)) # standard deviation of sample size of studies
+    odds_ratio <- exp(or) # odds ratio
+    
+    
+    # generate sample sizes
+    sample_sizes <-
+      round(rlnorm(n_studies, meanlog = sample_size_mean, sdlog = sample_size_sd),
+            0)
+    
+    # create emtpy data frame and fill with data
+    data <-
+      data.frame(
+        tpos = NA,
+        tneg = NA,
+        cpos = NA,
+        cneg = NA,
+        N = NA
+      )
+    
+    # loopediloop
+    for (i in 1:n_studies) {
+      data[i, ] <-
+        c(random_freqs(sample_sizes[i], 0.5, 0.5, odds_ratio),
+          sample_sizes[i])
+    }
+    
+    # add random publication date and author string
+    curr_year <-
+      as.numeric(format(Sys.Date(), "%Y"))
+    data$year <-
+      floor(runif(n_studies, min = 1900, max = curr_year))
+    data$author <- rep("Random et al.", n_studies)
+    return(data)
+    
+  }
   
-       output$dataChoiceUI <-  renderUI({
-         
-         if(dataC() == "1"){
-           
-         #Selection of a given Dataset
-                     selectInput("dataSelect", label = h3("Select a Dataset"),
-                     choices = as.list(data(package='metafor')$results[,3]),
-                     selected = 1)
-           
-         } else if (dataC() == "2"){
-           div(
-           # Input: Select a file ----
-           fileInput("file1", "Choose CSV File",
-                     multiple = FALSE,
-                     accept = c("text/csv",
-                                "text/comma-separated-values,text/plain",
-                                ".csv")),
-           
-           # Input: Checkbox if file has header ----
-           checkboxInput("header", "Header", TRUE),
-
-           # Input: Select separator ----
-            radioButtons("sep", "Separator",
-                         choices = c(Comma = ",",
-                                     Semicolon = ";",
-                                     Tab = "\t"),
-                         selected = ","),
-
-           # Input: Select quotes ----
-            radioButtons("quote", "Quote",
-                         choices = c(None = "",
-                                     "Double Quote" = '"',
-                                     "Single Quote" = "'"),
-                         selected = '"')
-
-           
-           
+  
+  dataC <- reactive({
+    input$dataChoice
+  })
+  
+  output$dataChoiceUI <-  renderUI({
+    if (dataC() == "1") {
+      # load example data
+    } else if (dataC() == "2") {
+      div(
+        # Input: Select a file ----
+        fileInput(
+          "file1",
+          "Choose CSV File",
+          multiple = FALSE,
+          accept = c(
+            "text/csv",
+            "text/comma-separated-values,text/plain",
+            ".csv"
           )
-           
-         } else if (dataC() == "3"){
-           
-           selectInput("dataSelect", label = h3("Select a Dataset"),
-                       choices = list("mtcars" = "mtcars"),
-                       selected = 1)
-         }
-         
-         # output$dataUpload <- reactive({
-         #   input$refresh
-         #   isolate({read.csv(input$file1$datapath,
-         #                     header = input$header,
-         #                     sep = input$sep,
-         #                     quote = input$quote)
-         #   })
-         # })
-         # observeEvent(input$refresh, once = T, {
-         #   dataUpload <- read.csv(input$file1$datapath,
-         #                                 header = input$header,
-         #                                 sep = input$sep,
-         #                                 quote = input$quote)
-         # })
-         
+        ),
+        
+        # Input: Checkbox if file has header ----
+        checkboxInput("header", "Header", TRUE),
+        
+        # Input: Select separator ----
+        radioButtons(
+          "sep",
+          "Separator",
+          choices = c(
+            Comma = ",",
+            Semicolon = ";",
+            Tab = "\t"
+          ),
+          selected = ","
+        ),
+        
+        # Input: Select quotes ----
+        radioButtons(
+          "quote",
+          "Quote",
+          choices = c(
+            None = "",
+            "Double Quote" = '"',
+            "Single Quote" = "'"
+          ),
+          selected = '"'
+        )
+        
+        
+        
+      )
+      
+    } else if (dataC() == "3") {
+      div(
+        sliderInput("n_studies", "Number of studies:", 4, 20, 12),
+        sliderInput(
+          "sample_size_mean",
+          "Mean sample size of studies:",
+          20,
+          2000,
+          990
+        ),
+        sliderInput("sample_size_sd", "SD of sample size of studies:", 0, 10, 5),
+        sliderInput("odds_ratio", "True odds ratio:", -3, 3, 0, step = 0.1)
+        
+      )
+      
+      
+      
+    }
+    
+  })
+  
+  dta <- reactive({
+    input$refresh
+    isolate({
+      if (dataC() == "1") {
+        # load example data set and select relevant rows and columns
+        data <- metafor::dat.bcg
+        data <- subset(data, alloc == "random")
+        data$N <- rowSums(data[, c(4, 5, 6, 7)])
+        data <- as.data.frame(data)
+        
+      } else if (dataC() == "2") {
+        read.csv(
+          input$file1$datapath,
+          header = input$header,
+          sep = input$sep,
+          quote = input$quote
+        )
+      } else if (dataC() == "3") {
+        simulate_data(
+          n = input$n_studies,
+          m = input$sample_size_mean,
+          sd = input$sample_size_sd,
+          or = input$odds_ratio
+        )
+      } else {
+        get(metafor::dat.bcg)
+      }
     })
-       
-       dta <- reactive({
-         input$refresh 
-         isolate({
-           if(dataC() == "1"){ 
-             get(input$dataSelect)
-           } else if (dataC() == "2"){
-             read.csv(input$file1$datapath,
-                            header = input$header,
-                            sep = input$sep,
-                            quote = input$quote)
-           } else if (dataC() == "3"){
-             get(input$dataSelect)
-           } else {
-             get(metafor::dat.bangertdrowns2004)
-           }
-         })
-       })
-       
+  })
+  
+  
+  output$dataUI <- DT::renderDataTable({
+    dta()
+  })
+  
+  observeEvent(input$refresh, {
+    showNotification("Data was updated!", type = "error", duration = 8)
+  })
+  
+  output$forest <- renderPlot({
+    data = dta()
+    
+    # Calculate random-effects model
+    res <- rma(
+      measure = "OR",
+      ai = tpos,
+      bi = tneg,
+      ci = cpos,
+      di = cneg,
+      data = data,
+      slab = paste(author, year, sep = ", ")
+    )
+    
+    # parameter to scale point sizes in regards to sample size
+    size <-
+      ((data$N - min(data$N)) / (max(data$N) - min(data$N)) * 2) + 1
+    
+    # plot basic forest plot with random effects model
+    forest(
+      res,
+      xlim = c(-8, 5),
+      order = "fit",
+      cex = .8,
+      ilab = data$N,
+      ilab.xpos = -4,
+      col = "#D3D3D3",
+      psize = size,
+      addcred = TRUE
+    )
+    
+    # switch to bold font for headers
+    op <- par(font = 2)
+    
+    # brute force method for size scaling to avoid overlapping of headers
+    # (for different number of studies)
+    if (nrow(data) <= 13) {
+      if (nrow(data) <= 6) {
+        n <- (nrow(data) * 1.3)
         
-       
-           # output$dataUI <- renderUI({
-           #   if (input$refresh == 0)
-           #     return()
-           #   output$dataTable <- renderTable(dta()) °
-           #   fluidRow(tableOutput("dataTable"))
-           # })
-           
-           
-        output$dataUI <- DT::renderDataTable({ dta() })
-        
-        observeEvent(input$refresh, {
-          showNotification("Data was updated!", type = "error", duration = 8)
-        })
-
+      } else{
+        n <- (nrow(data) * 1.2)
+      }
+    } else{
+      n <- (nrow(data) * 1.1)
+    }
+    
+    # add headers
+    text(-8, n  , "Author(s) and Year", pos = 4, cex = .8)
+    text(5, n , "Observed OR [95% CI]",  pos = 2, cex = .8)
+    text(-4, n , "N", cex = .8)
+  })
 })
-
-
